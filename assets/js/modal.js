11dabed6bb53b38233d895789155ae1333caffe3
Harold Zarco <harold.jzarco@gmail.com>
2022-07-12 01:28:23 -0600

    edit projects images

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,32 +1,8 @@
 function modal(id) {
-    id = id.replace('_before', '');
-    var modal = document.getElementById("myModal");
-
-    var img = document.getElementById(id);
-    var img1 = document.getElementById(id+'_before');
-
-    var modalImg = document.getElementById("after");
-    var modalImg1 = document.getElementById("before");
-
-    var title = document.getElementById("modal_title");
-    
-        modal.style.display = "block";
-        modalImg.src = img.src;
-        modalImg1.src = img1.src;
-        title.innerHTML = img.nextElementSibling.innerHTML
-
-
-    var span = document.getElementsByClassName("close")[0];
-
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
     var swiper = new Swiper(".mySwiper", {
         spaceBetween: 10,
         slidesPerView: 1,
         freeMode: true,
-        watchSlidesProgress: true,
     });
     var swiper2 = new Swiper(".mySwiper2", {
         spaceBetween: 10,
@@ -38,4 +14,19 @@ function modal(id) {
             swiper: swiper,
         },
     });
+
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("images_slide");
+    var title = document.getElementById("modal_title");
+    modalImg.innerHTML = '';
+    modalImg.innerHTML = id.firstElementChild.innerHTML;
+    modal.style.display = "block";
+    title.innerHTML = id.lastElementChild.innerHTML;
+
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
 }
